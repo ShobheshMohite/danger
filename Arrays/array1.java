47a7ff806b7b97bc8d23c244c012660161aa06ac
Shobhesh Sambhaji Mohite <shobheshsmohite007@gmail.com>
2024-11-19 12:11:16 +0530

    new

--- Arrays/array1.java
+++ Arrays/array1.java
@@ -2,9 +2,7 @@ package Arrays;
 
 public class array1 {
   public static void main(String[] args) {
-    int[] a = new int[5];
-    a[0] = 15;
+    int[] a = new int[] { 15, 20, 38, 89, 76 };
     System.out.println(a[0]);
   }
-
 }
