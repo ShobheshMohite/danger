cb3091bd66f1787f16fb74ad991b16ef6c3405d1
Shobhesh Sambhaji Mohite <shobheshsmohite007@gmail.com>
2024-11-19 11:43:08 +0530

    Arrays

--- NumberPattern14.java
+++ NumberPattern14.java
@@ -1,8 +1,10 @@
-public class NumberPattern13 {
+public class NumberPattern14 {
   public static void main(String[] args) {
+    int count = 0;
     for (int i = 1; i <= 5; i++) {
       for (int j = 1; j <= i; j++) {
-        System.out.print(i + " ");
+        count++;
+        System.out.print(count + " ");
       }
       System.out.println();
     }
